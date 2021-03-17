22b5d915df71cefb86a45d63557b6ef761ffb053
Oo-Uu <fbduddn97@naver.com>
2021-03-17 20:02:33 +0900

    first init

---
+++ src/main/java/com/where/WhereYouAt/WhereYouAtApplication.java
@@ -0,0 +1,13 @@
+package com.where.WhereYouAt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereYouAtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereYouAtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/where/WhereYouAt/WhereYouAtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.where.WhereYouAt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereYouAtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
